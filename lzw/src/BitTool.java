13428f2190340e130a83a6a8847dbe4f7ed2ab4e
kystyn <konstantindamaskinskiy@gmail.com>
2019-09-27 00:50:27 +0300

    started lzw

---
+++ lzw/src/BitTool.java
@@ -0,0 +1,4 @@
+public class BitTool
+{
+    public static int bulkSize = 1 << 8;
+}
---
+++ lzw/src/Entry.java
@@ -0,0 +1,7 @@
+class Entry
+{
+    public static void main( String[] Args )
+    {
+        System.out.println("Hello!");
+    }
+}
