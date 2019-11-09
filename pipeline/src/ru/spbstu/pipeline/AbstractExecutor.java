a1870fbe455806221f21328f69a064a94a55759c
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2019-11-10 01:44:46 +0300

    Lists of other executors removed from AnstractExecutor

--- pipeline/src/ru/spbstu/pipeline/AbstractExecutor.java
+++ pipeline/src/ru/spbstu/pipeline/AbstractExecutor.java
@@ -7,14 +7,11 @@ import java.util.List;
 
 public abstract class AbstractExecutor implements Executor {
 
-    protected final List<Executor> producers = new ArrayList<Executor>();
-    protected final List<Runnable> consumers = new ArrayList<Runnable>();
     protected final Logger logger;
-
     protected Status status;
     protected byte[] result;
 
-    AbstractExecutor(Logger logger) {
+    protected AbstractExecutor(Logger logger) {
         this.logger = logger;
     }
 
@@ -26,15 +23,5 @@ public abstract class AbstractExecutor implements Executor {
     @Override
     public final byte[] get() {
         return result;
-    }
-
-    @Override
-    public final void addProducer(Executor producer) {
-        producers.add(producer);
-    }
-
-    @Override
-    public final void addConsumer(Runnable consumer) {
-        consumers.add(consumer);
     }
 }
