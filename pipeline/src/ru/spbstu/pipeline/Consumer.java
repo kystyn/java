a432d1d331bf1226b00a587cedd862164e187047
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2019-11-23 23:25:09 +0300

    Set of acceptable types can be got

--- pipeline/src/ru/spbstu/pipeline/Consumer.java
+++ pipeline/src/ru/spbstu/pipeline/Consumer.java
@@ -1,6 +1,18 @@
 package ru.spbstu.pipeline;
 
+import java.util.Collections;
+import java.util.Set;
+
 public interface Consumer extends Fallible, Runnable {
+
+    /**
+     * Types of input data consumer can work with.
+     * Class full name is for example String.class.getCanonicalName().
+     * @return Set of full names of accessible input types for consumer.
+     */
+    default Set<String> acceptableInputTypes() {
+        return Collections.singleton(byte[].class.getCanonicalName());
+    }
 
     /**
      * Checks producer status and loads ready data from it if OK.
