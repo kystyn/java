dc6ac763a72f6543f07725257d44ffde2ef1daa9
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2019-11-23 23:24:28 +0300

    get return type is now optional

--- pipeline/src/ru/spbstu/pipeline/Producer.java
+++ pipeline/src/ru/spbstu/pipeline/Producer.java
@@ -1,9 +1,12 @@
 package ru.spbstu.pipeline;
 
+import java.util.Optional;
+
 public interface Producer extends Fallible {
 
     /**
-     * @return Byte array of even length.
+     * @return Data with type from list of consumer's acceptable types
+     * or empty if neither type is supported.
      */
-    Object get();
+    Optional<Object> get();
 }
