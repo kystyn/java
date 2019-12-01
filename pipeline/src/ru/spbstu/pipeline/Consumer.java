0c0bb9b65bb45bf14a919875dea9fd36c504c47c
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2019-12-02 00:37:04 +0300

    Doc loading fixed

--- pipeline/src/ru/spbstu/pipeline/Consumer.java
+++ pipeline/src/ru/spbstu/pipeline/Consumer.java
@@ -4,6 +4,7 @@ public interface Consumer extends Fallible, Runnable {
 
     /**
      * Checks producer status and loads ready data from it if success.
+     * Loading data means copying to inner buffer.
      *
      * @return Length of loaded data part from the beginning of provided buffer.
      */
