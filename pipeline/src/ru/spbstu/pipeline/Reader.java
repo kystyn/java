a762b6a3dbf3f9b0dee32253dc78f42afcbe88d9
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2019-12-09 14:03:15 +0300

    Update Reader.java

--- pipeline/src/ru/spbstu/pipeline/Reader.java
+++ pipeline/src/ru/spbstu/pipeline/Reader.java
@@ -3,7 +3,6 @@ package ru.spbstu.pipeline;
 /**
  * <ul>
  *     <li>Reads data in loop.</li>
- *     <li>Places status INPUT_STREAM_END at the end, and calls consumers last time.</li>
  * </ul>
  */
 public interface Reader extends Producer, InitializableProducer, Runnable {
